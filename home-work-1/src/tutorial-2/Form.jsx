813f834b8b4bba51976c92baea91609f9dcd9233
Artemiy <klimhammet62@gmail.com>
2021-11-27 16:37:06 +0300

    Сделал №2

---
+++ home-work-1/src/tutorial-2/Form.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <form>
+            <input type="text" placeholder="E-mail" />
+            <input type="text" placeholder="Пароль" />
+            <button>
+                <b>
+                    Войти
+                </b>
+            </button>
+        </form>
+    );
+};
+
+export default Form;
