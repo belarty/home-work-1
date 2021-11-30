e24106ed35d3f9d33d0cd030e8560da9c2596755
Artemiy <klimhammet62@gmail.com>
2021-11-30 23:39:21 +0300

    Задание №3

--- home-work-1/src/tutorial-3/App.js
+++ home-work-1/src/tutorial-3/App.js
@@ -1,30 +1,33 @@
-import React from "react";
-
-class TodoApp extends React.Component {
-    render() {
-        let inputValue = ['fefeaf'];
-        let searchValue;
-        let ul = document.getElementById('ul_elem');
-        function moveTask() {
-            inputValue.push(searchValue);
-            debugger
-            inputValue.forEach(element => {
-                ul.innerHTML += `
-                <li>
-                ${element}
-                </li>
-                `;
-            }
-            );
-            document.querySelector('input').value = '';
-        }            
-
-        return <div>
-            <input value={searchValue} placeholder='Введите задачу'/>
-            <button onClick={moveTask}>Добавить задачу</button>
-            <ul id='ul_elem'>{inputValue}</ul>
-        </div>;
+import React from 'react';
+class App extends React.Component {
+    function moveTask() {
+    let inputValue = ['fefeaf'];
+let searchValue;
+let ul = document.querySelector('#ul_elem');
+ul = ['gfsfsa'];
+    inputValue.push(searchValue);
+    inputValue.forEach(element => {
+        ul.innerHTML += `
+            <li>
+            ${element}
+            </li>
+            `;
     }
+    );
+    document.querySelector('input').value = '';
+}
+render() {
+    return <div>
+        <input value={searchValue} placeholder='Введите задачу' />
+        <button onClick={moveTask}>Добавить задачу</button>
+        <ul id='ul_elem'>asda{inputValue}</ul>
+    </div>;
+}
 }
 
-export default TodoApp;
+export default App;
+
+
+
+
+
