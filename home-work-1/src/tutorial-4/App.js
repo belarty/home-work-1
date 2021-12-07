1fa2ffd63384ebe2eb4c9a8738a739d8eed960de
Artemiy <klimhammet62@gmail.com>
2021-12-07 15:09:56 +0300

    Сделал №4

--- home-work-1/src/tutorial-4/App.js
+++ home-work-1/src/tutorial-4/App.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import EmptyBlock from './components/EmptyBlock';
 import Phrase from './components/Phrase';
 
 const App = () => {
     const [items, setItems] = useState([]);
-    const [generateWord, setGenerateWord] = useState('');
-    const [mainList, setGenerateList] = useState([]);
-    
+
     const adjectivesArr = [
         "абсолютный",
         "адский",
@@ -52,27 +50,26 @@ const App = () => {
         "дурак"
     ];
 
-    let handleClickGenerate = () => {
-        setGenerateWord((adjectivesArr[1] + ' ' + adjectivesArr[1] + ' ' + nounsArr[2]));
-        setItems([items, ...generateWord]);
-        setGenerateWord([]);
+    let generatePhrase = () => {
+        let phrase = `${adjectivesArr[Math.floor(Math.random() * 29)]} ${adjectivesArr[Math.floor(Math.random() * 29)]} ${nounsArr[Math.floor(Math.random() * 10)]}`
+        
+        return phrase;
     }
-    useEffect(() => {
-        return () => {
-            setItems([items, ...generateWord]);
-        };
-    }, [generateWord]);
-    
+
+    let handleClickGenerate = () => {
+        setItems([generatePhrase(), ...items]);
+    }
+    //Так я не понял, у меня один стейт должен быть, но в него массив фраз поместить? А как
 
     let handleClickRemove = () => {
         setItems([]);
     }
-    
+
 
     return (
         <div>
             <div className='wrapper'>
-                {<EmptyBlock /> || <Phrase generateWord={generateWord} />}
+                {items.length === 0 ? <EmptyBlock /> : <Phrase />}
                 <ul>{items.map((name) => <li>{name}</li>)}</ul>
                 <button onClick={handleClickGenerate} className='btn btn_generate'>Сгенерировать</button>
                 <button onClick={handleClickRemove} className='btn btn_clear'>Очистить</button>
