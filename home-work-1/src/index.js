9d2c5aa25c702b490a7c8f7be47daa7906b181b2
Artemiy <klimhammet62@gmail.com>
2022-01-16 00:06:42 +0300

    Modal + deleteTask

--- home-work-1/src/index.js
+++ home-work-1/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './tutorial-8/App1';
+import App1 from './tutorial-8/App1';
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+      <App1 />
   </React.StrictMode>,
   document.getElementById('root')
 );
